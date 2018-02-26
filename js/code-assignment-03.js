de938d0547ac040f2f3dafbe9872545d2b109233
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-02-26 15:57:21 -0600

    Code 3.1

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -1,17 +1,17 @@
-let projectYear = 2018;
+let schoolYear = 2018;
 let projectString = 'Course assignments for GEOG 4046.';
 let projectThumb = true;
 let myArray = ['Book 1', 'Book 2', 'Book 3'];
-let projectTitle = ['Story Tour', 'Earthquake Activity Map'];
+let projectTitle = ['Story Tour' , 'Earthquake Activity Map' , 'Suspect Report' , 'Watershed Sinks in East Baton Rouge Parish' , 'U.S. Population Growth'];
 let projectUrl = [
   'https://www.arcgis.com/apps/MapTour/index.html?appid=3ffdce6b16ad4ff19132d6a8a2f6f932',
   'https://www.arcgis.com/apps/Styler/index.html?appid=ad8981442a6145c892c9b552e46434a5',
   'https://lsu-gis.maps.arcgis.com/apps/GeoForm/index.html?appid=426bd166ea73481e815e02e69faf716f'];
 
-console.log(projectYear);
+console.log(schoolYear);
 console.log(projectString);
 console.log(projectThumb);
 console.log(myArray);
-console.log('Title: ' + projectTitle);
-console.log('Assignment: ' + myArray[0] + ":" + projectTitle[0]);
+console.log('Title: ' + projectTitle[0]);
+console.log('Assignment: ' + myArray[0] + "--" + projectTitle[0]);
 console.log('URL: ' + projectUrl[0]);
