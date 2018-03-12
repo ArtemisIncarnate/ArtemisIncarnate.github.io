3e37169e166c9a6f595c19dfcf795e54951e9f29
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-03-12 18:39:32 -0500

    Class 3-12-18

--- js/code-assignment-06a.js
+++ js/code-assignment-06a.js
@@ -17,6 +17,8 @@ let webApps = [
   webApp1
 ]
 
+let webApps = [] // this is a global variable which is accessable in functions but does not need to refer to a parameter. Not a good practice to keep.
+
 for (let i = 0; i < webApps.length; i++) {
   console.log('webApp ' + i + ' title: ' + webApps[i].title)
   console.log('url: ' + webApps[i].url + ' Link to app')
@@ -36,4 +38,35 @@ for (let i = 0; i < webApps.length; i++) {
   } else {
     console.log('This assignment has title ' + i)
   }
+}
+
+function describeFruit (fruit) {
+  let description = 'The ' + fruit + 'is a delicious snack.'
+  return description
+}
+
+let fruitDescription = describeFruit('apple')
+console.log(fruitDescription)
+
+for (let i = 0; i < webApps.length; i++) {
+  let title = createTitle(i)
+  console.log(title)
+  let imageSrc = createImageSrc(i)
+  console.log(imageSrc)
+}
+
+function createTitle (i) {
+  return webApps[i].title
+}
+
+function createImageSrc () {
+  if (webApps[i].thumb === true) {
+    console.log('images/ss-webApps-' + webApps[i].id + '.PNG') // change projects to webApps and add correct image links
+  } else {
+    console.log('images/no-preview.png')
+  }
+}
+
+function createTitle () {
+  console.log('Index position ' + i + ' title: ' + webApps[i].title)
 }
