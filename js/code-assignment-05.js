c111689fd1af30b394f51ce0f62882ba6e5d046f
ArtemisIncarnate <barane1@lsu.edu>
2018-03-12 10:30:22 -0500

    Code 5

--- js/code-assignment-05.js
+++ js/code-assignment-05.js
@@ -20,9 +20,13 @@ let webApps = [
 for (let i = 0; i < webApps.length; i++) {
   console.log('webApp ' + i + ' title: ' + webApps[i].title)
   console.log('url: ' + webApps[i].url + ' Link to app')
+  console.log('images/ss-project-' + webApps[i].id + '.png')
   if (webApps[i].url === true) {
   console.log('url ' + webApps[i].url + 'Success!')
   } else {
     console.log('url/no-preview.com')
+  }
+  if (projects[i].hasThumbnail === true) {
+  console.log('images/ss-project-' + webApps[i].id + '.png')
   }
 }
