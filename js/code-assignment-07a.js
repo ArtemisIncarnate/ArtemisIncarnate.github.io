cbab5b1796dc1e1aa7be194725bca294a49c0855
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-03-18 23:47:23 -0500

    Code 7 A

---
+++ js/code-assignment-07a.js
@@ -0,0 +1,10 @@
+//this library contains code used in GEOG 4046
+
+//function describeProject (project) {
+  //let projectDescription = 'The ' + myProject + ' is one of many in a student portfolio.'
+  //return projectDescription
+//}
+
+//let myProject = 'Book Assignment 2, Earthquake Activity Map, '
+//let projectDescription = describeProject('myProject')
+//console.log(projectDescription)
