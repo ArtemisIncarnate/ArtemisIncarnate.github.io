9ac9ee571cf07a30bfd3a83e01262bb3cde85fc2
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-03-19 16:18:09 -0500

    Code

--- js/code-library.js
+++ js/code-library.js
@@ -1,15 +1,15 @@
-//this library contains code used in GEOG 4046
-//<script src = "js/code-library.js"></script>
+ // this library contains code used in GEOG 4046
+ // <script src = "js/code-library.js"></script>
 
-//this will become a library of code. This is referring to a bunch of functions or
-//classes that can be used for something else.
-//this library file (used for the portfolio page) saves one from having to write their
+// this will become a library of code. This is referring to a bunch of functions or
+// classes that can be used for something else.
+// this library file (used for the portfolio page) saves one from having to write their
 // own code. We can use the on-file library codes to reference and use a code that has
 // already been made.
 // Library is a collection of code around a certain theme. (i.e. a mapping library for ArcGIS)
 
-//jQuery code - way of doing something after it has already been run and waiting for it to load again
-//$(document).ready(console.log('The page is ready!'))  // this is not built into Javascript. It is built into our jQuery library
+// jQuery code - way of doing something after it has already been run and waiting for it to load again
+// $(document).ready(console.log('The page is ready!'))  // this is not built into Javascript. It is built into our jQuery library
 
 let myProjects = 'Book Assignment 1, Story Tour, '
 $(document).ready(describeProject('myProjects'))
--- js/library2.js
+++ js/library2.js
@@ -1,3 +1,4 @@
+// this library is for notes taking
 // <script src = "js/library2.js"></script>
 // this will become a library of code. This is referring to a bunch of functions or
 // classes that can be used for something else.
@@ -7,4 +8,4 @@
 // Library is a collection of code around a certain theme. (i.e. a mapping library for ArcGIS)
 
 // jQuery code - way of doing something after it has already been run and waiting for it to load again
-$(document).ready(console.lof('The page is ready'))  // this is not built into Javascript. It is built into our jQuery library
+// $(document).ready(console.lof('The page is ready'))  // this is not built into Javascript. It is built into our jQuery library
