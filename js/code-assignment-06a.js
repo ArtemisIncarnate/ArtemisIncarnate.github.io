0903d7dfce0bdc14fec3aff1e8e5a7fde1619bdb
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-03-19 16:15:43 -0500

    Code

--- js/code-assignment-06a.js
+++ js/code-assignment-06a.js
@@ -1,49 +1,49 @@
-//the following 16 lines of code are set as comments so that errors will be averted in the console.
+// the following 16 lines of code are set as comments so that errors will be averted in the console.
 
-//let webApp0 = {
-  //id: 0,
-  //title: 'Web Map App: Client',
-  //url: 'https://lsu-gis.maps.arcgis.com/apps/webappviewer/index.html?id=4e483548f9c246109f2c044798951275',
-  //hasThumbnail: true
-//}
+// let webApp0 = {
+  // id: 0,
+  // title: 'Web Map App: Client',
+  // url: 'https://lsu-gis.maps.arcgis.com/apps/webappviewer/index.html?id=4e483548f9c246109f2c044798951275',
+  // hasThumbnail: true
+// }
 
-//let webApp1 = {
-  //id: 1,
-  //title: 'Web Map App: User',
-  //url: 'https://lsu-gis.maps.arcgis.com/apps/webappviewer/index.html?id=84b8637888554febb96bdf2c9580f07a',
-  //hasThumbnail: true
-//}
+// let webApp1 = {
+  // id: 1,
+  // title: 'Web Map App: User',
+  // url: 'https://lsu-gis.maps.arcgis.com/apps/webappviewer/index.html?id=84b8637888554febb96bdf2c9580f07a',
+  // hasThumbnail: true
+// }
 
-//let webApps = [
-  //webApp0,
-  //webApp1
-//]
+// let webApps = [
+  // webApp0,
+  // webApp1
+// ]
 
-//let webApps = []
-//this is a global variable which is accessable in functions but does not need to refer to a parameter. Not a good practice to keep.
+// let webApps = []
+// this is a global variable which is accessable in functions but does not need to refer to a parameter. Not a good practice to keep.
 
-//for (let i = 0; i < webApps.length; i++) {
-  //console.log('webApp ' + i + ' title: ' + webApps[i].title)
-  //console.log('url: ' + webApps[i].url + ' Link to app')
-  //console.log('images/screenviewhome00' + webApps[i].id + '.png')
-  //if (webApps[i].url === true) {
-    //console.log('url ' + webApps[i].url + 'Success!')
-  //} else {
-    //console.log('url/no-preview.com')
-  //}
-  //if (webApps[i].hasThumbnail === true) {
-    //console.log('images/screenviewhome00' + webApps[i].id + '.png')
-  //} else {
-    //console.log('images/no-preview.com')
-  //}
-  //if (webApps[i].title === '') {
-    //console.log('The title is Assignment ' + i)
-  //} else {
-    //console.log('This assignment has title ' + i)
-  //}
-//}
+// for (let i = 0; i < webApps.length; i++) {
+  // console.log('webApp ' + i + ' title: ' + webApps[i].title)
+  // console.log('url: ' + webApps[i].url + ' Link to app')
+  // console.log('images/screenviewhome00' + webApps[i].id + '.png')
+  // if (webApps[i].url === true) {
+    // console.log('url ' + webApps[i].url + 'Success!')
+  // } else {
+    // console.log('url/no-preview.com')
+  // }
+  // if (webApps[i].hasThumbnail === true) {
+    // console.log('images/screenviewhome00' + webApps[i].id + '.png')
+  // } else {
+    // console.log('images/no-preview.com')
+  // }
+  // if (webApps[i].title === '') {
+    // console.log('The title is Assignment ' + i)
+  // } else {
+    // console.log('This assignment has title ' + i)
+  // }
+// }
 
-//begin js.code-assignment-06 specific code
+// begin js.code-assignment-06 specific code
 
 function describeProject (project) {
   let projectDescription = 'The ' + myProject + ' is one of many in a student portfolio.'
@@ -54,5 +54,5 @@ let myProject = 'Book Assignment 2, Earthquake Activity Map, '
 let projectDescription = describeProject('myProject')
 console.log(projectDescription)
 
-//this is also the same as 1) let projectDescription = describeProject('myProject'); 2) console.log(projectDescription)
+// this is also the same as 1) let projectDescription = describeProject('myProject'); 2) console.log(projectDescription)
 // this is also the same as 1) console.log(describeProject('myProject'))
