2ce1d39c6b26cef1845e1641e40f9df2d8826c38
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-04-02 12:31:50 -0500

    Map 01

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,5 +1,5 @@
 // let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
-let mymap = L.map('drawMap').setView([51.499552, -0.099892], 14.25) // this map is being created as an object
+let mymap = L.map('drawMap').setView([51.499552, -0.099892], 14) // this map is being created as an object
 // the capitol L here is specfiic to Leaflet and the syntax is specific to it (like console is for js
 // the dot.setview is another function that will take the map object and rearranges aspects of the object function
 // 13 is the zoom level. The higher the number, the more zoomed in it is. It can go up to 20
