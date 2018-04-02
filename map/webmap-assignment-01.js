e15892b0a3804a61420759206aae339a0a90f9f9
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-04-02 13:58:16 -0500

    Map 1

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,16 +1,16 @@
-let mymap = L.map('drawMap').setView([51.505, -0.09], 14)
+let mymap = L.map('drawMap').setView([51.499, -0.08], 14)
 
 let basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
 L.tileLayer(basemapUrl).addTo(mymap)
 
-let marker = L.marker([51.5, -0.09]).addTo(mymap)
+let marker = L.marker([51.508077, -0.096854]).addTo(mymap)
 let polygon = L.polygon([
   [51.509, -0.08],
   [51.503, -0.06],
   [51.51, -0.047]
 ], {color: 'lightgreen'}).addTo(mymap)
 
-marker.bindPopup('London Bridge Station')
+marker.bindPopup('Shakespeare\'s Globe Theatre')
 polygon.bindPopup('St. Katharine\'s and Wapping')
 
 function logCurrentCoordinates (event) {
