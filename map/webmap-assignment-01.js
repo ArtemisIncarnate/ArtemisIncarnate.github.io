ea9e4db4fb96b14fc0a41c0a23d52162268aabef
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-04-02 13:53:57 -0500

    Map 0101

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,27 +1,17 @@
-var siteseeing = [
-		["London Bridge Station",51.5,-0.09],
-		["Big Ben",51.508155,-0.099892],
-		["Westminster Abbey",-51.499552,-0.126971],
-		["Shakespeare\'s Globe Theatre",51.508077,-0.096854]
-		]
-
 let mymap = L.map('drawMap').setView([51.505, -0.09], 14)
 
 let basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
 L.tileLayer(basemapUrl).addTo(mymap)
 
+let marker = L.marker([51.5, -0.09]).addTo(mymap)
 let polygon = L.polygon([
   [51.509, -0.08],
   [51.503, -0.06],
   [51.51, -0.047]
 ], {color: 'lightgreen'}).addTo(mymap)
 
+marker.bindPopup('London Bridge Station')
 polygon.bindPopup('St. Katharine\'s and Wapping')
-
-for (var i = 0; i < siteseeing.length; i++) {
-  marker = new L.marker([planes[i][1],planes[i][2]])
-    .bindPopup(siteseeing[i][0])
-    .addTo(mymap);
 
 function logCurrentCoordinates (event) {
   console.log('You clicked the map at ' + event.latlng)
