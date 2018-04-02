65337a5335ef64a1c88df30448b33792ecfe31ab
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-04-02 13:12:08 -0500

    Map 01

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,24 +1,23 @@
-// let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
-let mymap = L.map('drawMap').setView([51.499, -0.007], 14) // this map is being created as an object
-// the capitol L here is specfiic to Leaflet and the syntax is specific to it (like console is for js
-// the dot.setview is another function that will take the map object and rearranges aspects of the object function
-// 13 is the zoom level. The higher the number, the more zoomed in it is. It can go up to 20
-
-// 51.499552, -0.099892], 14
-// 51.508155, -0.099892 - Big Ben
-// 51.499552, -0.126971 - Westminster Abbey
+let mymap = L.map('drawMap').setView([51.499, -0.1], 14)
 
 let basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
 L.tileLayer(basemapUrl).addTo(mymap)
-// let markerArray = [5.15, -0.09] -- this is an alternative way of writing code below
-let marker = L.marker([51.5, -0.09]).addTo(mymap)
+
+let markerTransport = L.marker([51.5, -0.09]).addTo(mymap)  // class default - London Bridge Station
+let markerClock = L.marker([51.508155, -0.099892]).addTo(mymap) // Big Ben
+let markerAbbey = L.marker([51.499552, -0.126971]).addTo(mymap) // Westminster Abbey
+let markerTheatre = L.marker([51.508077, -0.096854]) // Globe Theatre
+
 let polygon = L.polygon([
   [51.509, -0.08],
   [51.503, -0.06],
   [51.51, -0.047]
 ], {color: 'lightgreen'}).addTo(mymap)
 
-marker.bindPopup('London Bridge Station')
+markerTransport.bindPopup('London Bridge Station')
+markerClock.bindPopup('Big Ben')
+markerAbbey.bindPopup('Westminster Abbey')
+markerTheatre.bindPopup('Shakespeare\'s Globe Theatre)
 polygon.bindPopup('St. Katharine\'s and Wapping')
 
 function logCurrentCoordinates (event) {
