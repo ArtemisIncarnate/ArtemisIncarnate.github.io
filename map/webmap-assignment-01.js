6fc47471cc883c40b58cc531de519af6901adaff
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-04-02 14:21:30 -0500

    Map red 01

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,4 +1,4 @@
-let mymap = L.map('drawMap').setView([51.499, -0.095], 13.5)
+let mymap = L.map('drawMap').setView([51.499, -0.1], 13.5)
 
 let basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
 L.tileLayer(basemapUrl).addTo(mymap)
@@ -24,4 +24,4 @@ let latlngs = [
     [51.508077, -0.096854],
     [51.5, -0.09]
 ]
-let polyline = L.polyline(latlngs, {color: 'purple'}).addTo(mymap)
+let polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap)
