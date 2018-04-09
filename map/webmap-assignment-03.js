d66d000b00b202ae6de85463ede3c6b45783f098
ArtemisIncarnate <barane1@lsu.edu>
2018-04-09 11:51:20 -0500

    Webmap 3

--- map/webmap-assignment-03.js
+++ map/webmap-assignment-03.js
@@ -4,7 +4,7 @@ let newbasemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_
 L.tileLayer(newbasemapUrl).addTo(newmap)
 
 let myFlagIcon = L.icon({
-  iconFlagUrl: 'ukflagicon.png',
+  iconFlagUrl: 'Images/ukflagicon.png',
   iconFlagSize: [38, 95], // size of the icon
   iconFlagAnchor: [51.499, -0.127], // point of the icon which will correspond to marker's location
   popupFlagAnchor: [-50, -0.135] // point from which the popup should open relative to the iconAnchor
@@ -21,5 +21,6 @@ let myNewPolygon = L.polygon([
 
 let myNewPolygonStyle = {
   color: 'blue',
-  fillColor: 'lightblue'
+  fillColor: 'lightblue',
+  fillOpacity: 0.3 
 }
