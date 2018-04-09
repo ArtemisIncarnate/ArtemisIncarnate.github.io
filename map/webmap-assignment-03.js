379f66009f5cb020e36b9bc74435b511f5bc4447
ArtemisIncarnate <barane1@lsu.edu>
2018-04-09 11:15:41 -0500

    Webmap 3

--- map/webmap-assignment-03.js
+++ map/webmap-assignment-03.js
@@ -3,29 +3,21 @@ let mymap = L.map('drawMap').setView([51.507, -0.096], 14)
 let basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
 L.tileLayer(basemapUrl).addTo(mymap)
 
-let marker = L.marker([51.499, -0.127]).addTo(mymap)
-let polygon = L.polygon([
+let myMarker = L.marker([51.499, -0.127], {icon: myFlagIcon}).addTo(mymap)
+let myFlagIcon = L.icon({
+  iconFlagUrl: 'ukflagicon.png',
+  iconFlagSize: [38, 95], // size of the icon
+  iconFlagAnchor: [51.499, -0.127], // point of the icon which will correspond to marker's location
+  popupFlagAnchor: [-50, -0.135] // point from which the popup should open relative to the iconAnchor
+})
+
+let myPolygon = L.polygon(
   [51.499, -0.127],
   [51.508, -0.099],
   [51.500 -0.090],
-  [51.499 -0.127]
-], {color: 'red'}).addTo(mymap)
+  [51.499 -0.127], myPolygonStyle).addTo(mymap)
 
-let myFlagIcon = L.icon({
-  iconUrl: 'leaf-green.png',
-  shadowUrl: 'leaf-shadow.png',
-  iconSize: [38, 95], // size of the icon
-  shadowSize: [50, 64], // size of the shadow
-  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-  shadowAnchor: [4, 62],  // the same for the shadow
-  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-})
-
-let myMarker = L.marker([51.5, -0.09], {icon: myGreenIcon}).addTo(map);
-
-let myStyle = {
-  color: 'orange',
-  fillColor: 'yellow'
+let myPolygonStyle = {
+  color: 'blue',
+  fillColor: 'lightblue'
 }
-
-let myPolygon = L.polygon(myCoords, myStyle).addTo(mymap)
