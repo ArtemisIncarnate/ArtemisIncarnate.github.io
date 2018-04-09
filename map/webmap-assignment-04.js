1b30bbdacf8cf788df966e9d142e8ce7a46a65f1
ArtemisIncarnate <barane1@lsu.edu>
2018-04-09 11:19:27 -0500

    Webmap 3

--- map/webmap-assignment-04.js
+++ map/webmap-assignment-04.js
@@ -1,10 +1,10 @@
-let mymap = L.map('drawMap').setView([51.507, -0.096], 14)
+// let mymap = L.map('drawMap').setView([51.507, -0.096], 14)
 
-let basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-L.tileLayer(basemapUrl).addTo(mymap)
+// let basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+// L.tileLayer(basemapUrl).addTo(mymap)
 
-let myGeojson = {"type":"FeatureCollection"...} // use your own geojson
+// let myGeojson = {"type":"FeatureCollection"...} // use your own geojson
 
-L.geoJSON(myGeojson).addTo(myMap)
+// L.geoJSON(myGeojson).addTo(myMap)
 
-L.geoJSON(myGeojson, { style: { color: 'red' }}).addTo(myMap) // object 2 is the color or inner object; object 1 is the style with the object 2 within console.log(require('util').inspect(, { depth: null }))
+// L.geoJSON(myGeojson, { style: { color: 'red' }}).addTo(myMap) // object 2 is the color or inner object; object 1 is the style with the object 2 within console.log(require('util').inspect(, { depth: null }))
