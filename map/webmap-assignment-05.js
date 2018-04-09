b9b7a485c029beeb06285c4d02b3eb3dc6d26092
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-04-09 17:27:57 -0500

    Maps

--- map/webmap-assignment-05.js
+++ map/webmap-assignment-05.js
@@ -14,6 +14,14 @@ let satelliteBasemap = L.tileLayer('https://korona.geog.uni-heidelberg.de/tiles/
 
 let oceanBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}').addTo(mymap)
 
+let mhi = L.esri.dynamicMapLayer({
+  url: 'https://servoces.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Household_Income/MapServer',
+  opacity: 0.7
+}).addTo(mymap)
+
+// return 'Risk Level' + featureCollection.features[0].properties[2012 Median Household Income (ESRI);
+// https://esri.github.io/esri-leaflet/examples/customizing-popups.html
+
 
 let myBasemaps = {
     'Light basemap': grayBasemap,
