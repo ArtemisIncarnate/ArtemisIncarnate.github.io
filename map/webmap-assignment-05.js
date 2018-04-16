a99d3a4d87ab7c6682011a379970cff29d1c6bf9
ArtemisIncarnate <barane1@lsu.edu>
2018-04-16 10:43:05 -0500

    Map 5

--- map/webmap-assignment-05.js
+++ map/webmap-assignment-05.js
@@ -1,40 +1,41 @@
-
-//function map4 () {
-
-//}
-
-let mymap = L.map('map5').setView([39, -98], 4)
+// begin Map 5
+let mapFive = L.map('drawMap5').setView([39, -98], 4)
 mymap.on('click', function(event){
 
-}
+} // center of US coords
+
 //create basemap layer
-let grayBasemap = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(mymap)
+let openStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapFive)
 
-let satelliteBasemap = L.tileLayer('https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}').addTo(mymap)
+let grayBasemap = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png')
 
-let oceanBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}').addTo(mymap)
+let satelliteBasemap = L.tileLayer('https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}')
+
+let oceanBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}')
 
 let mhi = L.esri.dynamicMapLayer({
   url: 'https://servoces.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Household_Income/MapServer',
   opacity: 0.7
-}).addTo(mymap)
+}).addTo(mapFive)
 
 // return 'Risk Level' + featureCollection.features[0].properties[2012 Median Household Income (ESRI);
 // https://esri.github.io/esri-leaflet/examples/customizing-popups.html
 
-
 let myBasemaps = {
-    'Light basemap': grayBasemap,
-    'Satellite': satelliteBasemap,
-    'Ocean basemap': oceanBasemap
+    'Open Streets Map': openSteetMap,
+    'Basic Map': grayBasemap,
+    'Satellite Map': satelliteBasemap,
+    'Ocean Map': oceanBasemap
 }
 
-L.control.layers(myBasemaps).addTo(mymap)
+L.control.layers(myBasemaps).addTo(mapFive)
 
 function myStyle (feature) {
 
 }
-
 function myPopup (feature, layer) {
 
 }
@@ -45,3 +46,32 @@ let myOptions = {
 }
 
 L.geoJSON(stateDemographics, myOptions).addTo(mymap)
+
+
+myStyle = function (state) {
+  let genderFemale = state.properties.FEMALES // female population
+  let genderMale = state.properties.MALES // male population
+  let stateColor = 'red'
+  if (genderFemale > genderMale) {stateColor = 'Purple'} // girls are royalty
+  let myStyle = {
+    color: stateColor,
+    fillOpacity: 0.3,
+    dashArray: 3,
+    weight: 4
+  }
+  return myStyle
+}
+
+let myOptions = {
+  style: myStyle,
+  onEachFeature: stateInfo
+}
+
+function stateInfo (state, layer) {
+  let stateName = state.properties.STATE_NAME
+  let genderFemale = state.properties.FEMALES
+  let genderMale = state.properties.MALES
+  let statePopulation = state.properties.POPULATION
+  layer.bindPopup(stateName + ': Population by gender <br>Assigned female at birth:' + genderFemale + '<br>Assigned male at birth:' + genderMale + '<br>Total population in 2016: ' + statePopulation)
+}
+L.geoJSON(mygeoJsonMapFour, myOptions).addTo(mapFour) // object 2 is the color or inner object; object 1 is the style with the object 2 within console.log(require('util').inspect(, { depth: null }))
