c21a1e104b424e7cef022025f67a0dae936802f2
ArtemisIncarnate <35709888+ArtemisIncarnate@users.noreply.github.com>
2018-05-04 09:58:14 -0500

    ccSlrMap

--- ccslrimpacts/cc.slr.js
+++ ccslrimpacts/cc.slr.js
@@ -1,8 +1,8 @@
-let ccSlrMap = L.map('drawSlrMap').setView([38, -98], 4) // center of US coords
+let mapCcSlr = L.map('drawSlrMap').setView([38, -98], 4) // center of US coords
 let openStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
   maxZoom: 18,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(ccSlrMap)
+}).addTo(mapCcSlr)
 
 let grayBasemap = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png')
 let satelliteBasemap = L.tileLayer('https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}')
@@ -15,7 +15,7 @@ let baseMapsCcSlr = {
   'Ocean Map': oceanBasemap
 }
 
-L.control.layers(baseMapsCcSlr).addTo(ccSlrMap)
+L.control.layers(baseMapsCcSlr).addTo(mapCcSlr)
 
 let mapStyle = function (state) {
   let genderFemale = state.properties.FEMALES // female population
@@ -43,5 +43,5 @@ function stateInfo (state, layer) {
   let statePopulation = state.properties.POPULATION
   layer.bindPopup(stateName + ': Population by gender <br>Assigned female at birth:' + genderFemale + '<br>Assigned male at birth:' + genderMale + '<br>Total population in 2016: ' + statePopulation)
 }
-L.geoJSON(mygeoJsonMapFour, mapOptions).addTo(ccSlrMap) // object 2 is the color or inner object; object 1 is the style with the object 2 within console.log(require('util').inspect(, { depth: null }))
-// L.geoJSON(stateDemographics, mapOptions).addTo(ccSlrMap)
+L.geoJSON(mygeoJsonMapFour, mapOptions).addTo(mapCcSlr) // object 2 is the color or inner object; object 1 is the style with the object 2 within console.log(require('util').inspect(, { depth: null }))
+// L.geoJSON(stateDemographics, mapOptions).addTo(mapCcSlr)
